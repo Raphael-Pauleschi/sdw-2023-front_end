8d3266b2d990e5608c831b3bee2a12e27087ecc0
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-10-26 12:56:11 -0300

    Project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ListComponent } from './pages/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
